afb451a1eb7b6a65c4348e80dbf91f9e18886dbd
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-02-08 17:23:20 +0000

    edit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,29 +33,29 @@
 
 
 
-		<header>
-			<h2>GARRETT MUSAR</h2>
-		<div class="icons">
-			<img src={instagram} alt="instagram" class="icon" />
-			<img src={linkedin} alt="linkedin" class="icon" />
-			<img src={mail} alt="mail" class="icon" />
-			<img src={twitter} alt="twitter" class="icon" />
+		<header >
+			<h2 class="fade-2">GARRETT MUSAR</h2>
+		<div class="icons ">
+			<img src={instagram} alt="instagram" class="icon fade-3a" />
+			<img src={linkedin} alt="linkedin" class="icon fade-3b" />
+			<img src={mail} alt="mail" class="icon fade-3c" />
+			<img src={twitter} alt="twitter" class="icon fade-3d" />
 		</div>
 	</header>
 		<!-- <div class="square">
 
 		</div> -->
 	<picture>
-		<h1>Business&nbsp;In&nbsp;The&nbsp;Front</h1>
+		<h1 class="fade">Business&nbsp;In&nbsp;The&nbsp;Front</h1>
 		<!-- <source srcset={casual} type="image/webp"  /> -->
-		<img src={casual} alt="Headshot" class="headshot" />
+		<img src={casual} alt="Headshot" class="headshot fade-1" />
 	</picture>
 
 
-	<div class="entry">
-		<h2 class="arrow">$</h2>
-		<h2>COME ON BACK</h2>
-		<h2 class="arrow">$</h2>
+	<div class="entry color-change">
+		<!-- <h2 class="arrow">$</h2> -->
+		<h2 class="fade-5">COME ON BACK</h2>
+		<!-- <h2 class="arrow">$</h2> -->
 	</div>
 
 </section>
@@ -156,21 +156,23 @@
 	}
 
 	.entry {
-		width: 36vh;
-		margin-top: 1.5vh;
+		width: 48vh;
+		margin-top: 1vh;
 		height: 6vh;
 		background: var(--background);
 		border: double 6px var(--primary);
 
 		margin-bottom: 30px;
 
+
 		display: flex;
 		align-items: center;
 		justify-content: space-around;
 
-		color: var(--background);
+		color: var(--primary);
 		cursor: pointer;
 		/* box-shadow: 0 0 10px var(--primary); */
+
 	}
 
 	.entry:hover {
@@ -183,12 +185,15 @@
 		background: var(--background);
 		color: var(--primary);
 		border-color: var(--primary);
+
+		
 	}
 
 	h2 {
 		font-size: 15px;
 		color: inherit;
 		letter-spacing: 3px;
+		font-weight: 600;
 	}
 
 .arrow {
@@ -199,6 +204,139 @@
 
 .entry:hover .arrow {
 		color: var(--background);
+	}
+
+
+	.fade-1 {
+		opacity: 0;
+		-webkit-animation: fadein .1s ease-out .1s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein .1s ease-out .1s; /* Firefox < 16 */
+        -ms-animation: fadein .1s ease-out .1s; /* Internet Explorer */
+         -o-animation: fadein .1s ease-out .1s; /* Opera < 12.1 */
+            animation: fadein .1s ease-out .1s;
+						animation-fill-mode: forwards;
+	}
+
+	.fade-2 {
+		opacity: 0;
+		-webkit-animation: fadein .1s ease-out .2s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein .1s ease-out .2s; /* Firefox < 16 */
+        -ms-animation: fadein .1s ease-out .2s; /* Internet Explorer */
+         -o-animation: fadein .1s ease-out .2s; /* Opera < 12.1 */
+            animation: fadein .1s ease-out .2s;
+						animation-fill-mode: forwards;
+	}
+
+	.fade-3a {
+		opacity: 0;
+		-webkit-animation: fadein .1s ease-out .3s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein .1s ease-out .3s; /* Firefox < 16 */
+        -ms-animation: fadein .1s ease-out .3s; /* Internet Explorer */
+         -o-animation: fadein .1s ease-out .3s; /* Opera < 12.1 */
+            animation: fadein .1s ease-out .3s;
+						animation-fill-mode: forwards;
+	}
+
+	.fade-3b {
+		opacity: 0;
+		-webkit-animation: fadein .1s ease-out .333s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein .1s ease-out .333s; /* Firefox < 16 */
+        -ms-animation: fadein .1s ease-out .333s; /* Internet Explorer */
+         -o-animation: fadein .1s ease-out .333s; /* Opera < 12.1 */
+            animation: fadein .1s ease-out .333s;
+						animation-fill-mode: forwards;
+	}
+
+	.fade-3c {
+		opacity: 0;
+		-webkit-animation: fadein .1s ease-out .4s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein .1s ease-out .4s; /* Firefox < 16 */
+        -ms-animation: fadein .1s ease-out .4s; /* Internet Explorer */
+         -o-animation: fadein .1s ease-out .4s; /* Opera < 12.1 */
+            animation: fadein .1s ease-out .4s;
+						animation-fill-mode: forwards;
+	}
+
+	.fade-3d {
+		opacity: 0;
+		-webkit-animation: fadein .1s ease-out .45s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein .1s ease-out .45s; /* Firefox < 16 */
+        -ms-animation: fadein .1s ease-out .45s; /* Internet Explorer */
+         -o-animation: fadein .1s ease-out .45s; /* Opera < 12.1 */
+            animation: fadein .1s ease-out .45s;
+						animation-fill-mode: forwards;
+	}
+
+	.fade-4 {
+		opacity: 0;
+		-webkit-animation: fadein .1s ease-out .4s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein .1s ease-out .4s; /* Firefox < 16 */
+        -ms-animation: fadein .1s ease-out .4s; /* Internet Explorer */
+         -o-animation: fadein .1s ease-out .4s; /* Opera < 12.1 */
+            animation: fadein .1s ease-out .4s;
+						animation-fill-mode: forwards;
+	}
+
+	.color-change {
+		-webkit-animation: colorChange .1s ease-out .8s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: colorChange .1s ease-out .8s; /* Firefox < 16 */
+        -ms-animation: colorChange .1s ease-out .8s; /* Internet Explorer */
+         -o-animation: colorChange .1s ease-out .8s; /* Opera < 12.1 */
+            animation: colorChange .1s ease-out .8s;
+						animation-fill-mode: forwards;
+	}
+
+	.fade-5 {
+		opacity: 0;
+		-webkit-animation: fadein .1s ease-out 1s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein .1s ease-out 1s; /* Firefox < 16 */
+        -ms-animation: fadein .1s ease-out 1s; /* Internet Explorer */
+         -o-animation: fadein .1s ease-out 1s; /* Opera < 12.1 */
+            animation: fadein .1s ease-out 1s;
+						animation-fill-mode: forwards;
+	}
+
+	@keyframes colorChange {
+    from { 
+			background: var(--background); 
+			border-color: var(--primary);
+			color: var(--primary);
+		}
+    to   { 
+			background: var(--primary); 
+			border-color: var(--background);
+			color: var(--background);
+		}
+	}
+
+
+	@keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+	}
+
+	/* Firefox < 16 */
+	@-moz-keyframes fadein {
+			from { opacity: 0; }
+			to   { opacity: 1; }
+	}
+
+	/* Safari, Chrome and Opera > 12.1 */
+	@-webkit-keyframes fadein {
+			from { opacity: 0; }
+			to   { opacity: 1; }
+	}
+
+	/* Internet Explorer */
+	@-ms-keyframes fadein {
+			from { opacity: 0; }
+			to   { opacity: 1; }
+	}
+
+	/* Opera < 12.1 */
+	@-o-keyframes fadein {
+			from { opacity: 0; }
+			to   { opacity: 1; }
 	}
 
 
